f49b457b8f73b096e24f79454b796ec37cb7a02e
Kazbegi <84750518+kachmazovi@users.noreply.github.com>
2024-11-16 01:12:39 +0400

    add cards

--- src/app/features/services/services.component.ts
+++ src/app/features/services/services.component.ts
@@ -8,4 +8,6 @@ import { ServiceCardComponent } from '../../shared/components/service-card/servi
   templateUrl: './services.component.html',
   styleUrl: './services.component.scss',
 })
-export class ServicesComponent {}
+export class ServicesComponent {
+  public cards = Array.from({ length: 8 }, (_, i) => i + 1);
+}
