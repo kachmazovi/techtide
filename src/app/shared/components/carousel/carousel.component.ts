b6a506e3478d3ca4ec1b61ac6af811e899ae2fdb
Kazbegi <84750518+kachmazovi@users.noreply.github.com>
2024-11-15 05:25:46 +0400

    add other companies logos and animation

--- src/app/shared/components/carousel/carousel.component.ts
+++ src/app/shared/components/carousel/carousel.component.ts
@@ -1,7 +1,11 @@
+import { NgStyle } from '@angular/common';
 import {
   AfterViewInit,
   Component,
   ElementRef,
+  HostListener,
+  OnDestroy,
+  Renderer2,
   signal,
   ViewChild,
 } from '@angular/core';
@@ -9,30 +13,103 @@ import {
 @Component({
   selector: 'app-carousel',
   standalone: true,
-  imports: [],
+  imports: [NgStyle],
   templateUrl: './carousel.component.html',
-  styleUrl: './carousel.component.scss',
+  styleUrls: ['./carousel.component.scss'],
 })
-export class CarouselComponent implements AfterViewInit {
-  @ViewChild('slider') slider!: ElementRef;
+export class CarouselComponent implements AfterViewInit, OnDestroy {
+  @ViewChild('slider1') slider1!: ElementRef;
+  @ViewChild('slider2') slider2!: ElementRef;
 
-  public images = Array.from({ length: 10 });
-  public isHodling = signal(false);
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.parentInnerWidth =
+      this.slider1.nativeElement.parentElement.clientWidth;
+  }
+
+  public images = Array.from({ length: 10 }, (_, i) => i + 1);
+  public sliderWidth = this.images.length * 260;
+  public slider1PositionX = 0;
+  public slider2PositionX = 0;
+  public parentInnerWidth = 0;
+
+  private intervalId?: number;
+  private isHodling = signal(false);
+
+  constructor(private renderer: Renderer2) {}
 
   ngAfterViewInit(): void {
-    this.slider.nativeElement.style.width = `${this.images.length * 260}px`;
+    setTimeout(() => {
+      this.startAnimation();
+      this.renderer.setStyle(this.slider2.nativeElement, 'display', 'none');
+      this.parentInnerWidth =
+        this.slider1.nativeElement.parentElement.clientWidth;
+    }, 100);
   }
 
-  public onMouseDown() {
-    this.slider.nativeElement.style.cursor = 'grabbing';
-    console.log(this.slider);
+  ngOnDestroy(): void {
+    this.clearAnimation();
   }
 
-  public onMouseMove(event: MouseEvent) {
-    this.slider.nativeElement.style.cursor = 'grab';
+  public onMouseDown(): void {
+    this.setCursorStyle('grabbing');
+    this.isHodling.set(true);
+    this.clearAnimation();
   }
 
-  public onMouseUp() {
-    this.slider.nativeElement.style.cursor = 'grab';
+  public onMouseMove(): void {
+    this.setCursorStyle('grab');
+  }
+
+  public onMouseUp(): void {
+    this.setCursorStyle('grab');
+    this.isHodling.set(false);
+  }
+
+  private setCursorStyle(cursor: string): void {
+    this.renderer.setStyle(this.slider1.nativeElement, 'cursor', cursor);
+  }
+
+  private startAnimation(): void {
+    if (this.intervalId === undefined) {
+      this.slider2PositionX = this.getNextPositionX(this.slider1PositionX);
+      setTimeout(() => {
+        this.animateSliders();
+        this.intervalId = window.setInterval(() => {
+          this.animateSliders();
+        }, 2500);
+      }, 100);
+    }
+  }
+
+  private clearAnimation(): void {
+    if (this.intervalId !== undefined) {
+      clearInterval(this.intervalId);
+      this.intervalId = undefined;
+    }
+  }
+
+  private animateSliders(): void {
+    this.renderer.setStyle(this.slider1.nativeElement, 'display', 'flex');
+    this.renderer.setStyle(this.slider2.nativeElement, 'display', 'flex');
+
+    this.slider1PositionX -= 260;
+    this.slider2PositionX -= 260;
+
+    if (this.slider1PositionX < -this.sliderWidth) {
+      this.renderer.setStyle(this.slider1.nativeElement, 'display', 'none');
+      this.slider1PositionX = this.getNextPositionX(this.slider2PositionX);
+    }
+
+    if (this.slider2PositionX < -this.sliderWidth) {
+      this.renderer.setStyle(this.slider2.nativeElement, 'display', 'none');
+      this.slider2PositionX = this.getNextPositionX(this.slider1PositionX);
+    }
+  }
+
+  private getNextPositionX(currentPosition: number): number {
+    return this.sliderWidth > this.parentInnerWidth
+      ? currentPosition + this.sliderWidth
+      : currentPosition + this.parentInnerWidth + 260;
   }
 }
